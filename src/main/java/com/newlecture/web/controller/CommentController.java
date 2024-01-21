538f1e8fe6dff204f0645f34eb79e8b9e489538f
남병현 <kmi5063@naver.com>
2024-01-21 17:52:18 +0900

    댓글 추천 비추천 수정중2

--- src/main/java/com/newlecture/web/controller/CommentController.java
+++ src/main/java/com/newlecture/web/controller/CommentController.java
@@ -1,9 +1,8 @@
 package com.newlecture.web.controller;
 
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
+import java.util.*;
+
 import com.newlecture.web.entity.BoardEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,84 +48,52 @@ public class CommentController {
 
 		// 비추천누른 경우
 		if (cEnt.getComment_recommend() == null) {
-			// 이미 비추천을 눌렀다.
 
 
 			// 기존에 updown한 리스트에 아이디를 삭제하기
 			String downId = cEnt.getComment_ud_temp_id();
 			CommentEntity findComment = findOneComment(cEnt);
-
 			String up_list = findComment.getComment_up_id_list();
+			String list = findComment.getComment_down_id_list();
 
 			// 추천을 클릭해놓은 상태라면 지운다.
-			if (up_list != null && up_list.contains(downId)) {
+			if (up_list != null && up_list.contains(downId)) { // 추천이 눌려있는 경우
 
 				CommentEntity tempCent = new CommentEntity();
 				tempCent.setComment_idx(comment_idx);
 				tempCent.setComment_recommend("-1");
 				cDao.recommendUpDown(tempCent);
-				/*if(up_list.contains(downId)) {
-						up_list = up_list.replace("," + downId, "");
-				}*/
-				ArrayList<String> arr = new ArrayList<>();
-				arr.add(downId);
+				ArrayList<String> arr_up_list = new ArrayList<>(Arrays.asList(up_list));
 
-				for(int i = 0; i < arr.size(); i++){
-					String str = arr.get(i);
-					if(str.equals(downId)){
-						arr.remove(downId);
+				for (int i = 0; i < arr_up_list.size(); i++) {
+					System.out.println("1arr_up_list.size() = " + arr_up_list.size());
+                    System.out.println("str1111 = " + arr_up_list.get(i));
+//					arr_up_list.remove();
+					String str = arr_up_list.get(i);
+					arr_up_list.remove(str);
+//					System.out.println("str = " + arr_up_list.get(i));
 
-					}
-					up_list = String.join(",",arr);
+					up_list = String.join(",", arr_up_list);
 
-					/*if(up_list.contains(downId)){
-						up_list = up_list.replace( downId,  "");
-					}*/
 
-					System.out.println("1arr = " + arr);
-					System.out.println("1str = " + str);
-					System.out.println("1str.equals(downId) = " + str.equals(downId));
-					System.out.println("1arr.remove(downId)= " + arr.remove(downId));
-					System.out.println("1up_list = " + up_list);
-					System.out.println("1downId = " + downId);
-					System.out.println("1up_list.split(downId) = " + up_list.split(downId));
-					System.out.println("1up_list.contains(downId) = " + up_list.contains(downId));
-					System.out.println("1downId.contains(up_list) = " + downId.contains(up_list));
-					System.out.println("1up_list.equals(downId) = " + up_list.equals(downId));
-					System.out.println("1downId.equals(up_list) = " + downId.equals(up_list));
 				}
 
-				System.out.println("Arrays.toString(up_list.split(downId)" + Arrays.toString(up_list.split(downId)));
 				findComment.setComment_up_id_list(up_list);
 				findComment.setComment_down_id_list(null);
 				changeUpList(findComment);
 			}
 
-			String list = findComment.getComment_down_id_list();
 
-			if (list != null && list.contains(downId)) {
+			if (list != null && list.contains(downId)) { // 비추천이 눌려있는 경우
 				cEnt.setComment_unrecommend("-1");
 				cEnt.setComment_recommend(null);
 				cDao.recommendUpDown(cEnt);
-				ArrayList<String> arr = new ArrayList<>();
-				arr.add(downId);
-				for(int i = 0; i < arr.size(); i++) {
-					String str = arr.get(i);
-					if (str.equals(downId)) {
-						arr.remove(downId);
-					}
-					list = String.join(",", arr);
-					System.out.println("2arr = " + arr);
-					System.out.println("2str = " + str);
-					System.out.println("2str.equals(downId) = " + str.equals(downId));
-					System.out.println("2arr.remove(downId)= " + arr.remove(downId));
-					System.out.println("2up_list = " + up_list);
-					System.out.println("2downId = " + downId);
-					System.out.println("2up_list.split(downId) = " + up_list.split(downId));
-					System.out.println("2up_list.contains(downId) = " + up_list.contains(downId));
-					System.out.println("2downId.contains(up_list) = " + downId.contains(up_list));
-					System.out.println("up_list.equals(downId) = " + up_list.equals(downId));
-					System.out.println("21downId.equals(up_list) = " + downId.equals(up_list));
+				ArrayList<String> arr_list = new ArrayList<>(Arrays.asList(list));
+
+				for(int i = 0; i < arr_list.size(); i++) {
+					String str = arr_list.get(i);
+					arr_list.remove(str);
+					list = String.join(",", arr_list);
 				}
 				findComment.setComment_down_id_list(list);
 				findComment.setComment_up_id_list(null);
@@ -150,63 +117,38 @@ public class CommentController {
 			CommentEntity findComment = findOneComment(cEnt);
 
 			String down_list = findComment.getComment_down_id_list();
+			String list = findComment.getComment_up_id_list();
+			ArrayList<String> arr_down_list = new ArrayList<>(Arrays.asList(down_list));
+			ArrayList<String> arr_list = new ArrayList<>(Arrays.asList(list));
 
-			// 추천을 눌렀으나 비추천 누른게 있다면 지운다.
+			// 추천이 눌려있는 상태지만 비추천 누르면 추천을 지운다.
 			if (down_list != null && down_list.contains(upId)) {
 				CommentEntity resetDownEnt = new CommentEntity();
 				resetDownEnt.setComment_idx(comment_idx);
 				resetDownEnt.setComment_unrecommend("-1");
 				cDao.recommendUpDown(resetDownEnt);
-				ArrayList<String> arr = new ArrayList<>();
-				arr.add(upId);
-				for(int i = 0; i < arr.size(); i++) {
-					String str = arr.get(i);
-					if (str.equals(upId)) {
-						arr.remove(upId);
-					}
-					down_list = String.join(",", arr);
-					System.out.println("3arr = " + arr);
-					System.out.println("3str = " + str);
-					System.out.println("3str.equals(downId) = " + str.equals(upId));
-					System.out.println("3arr.remove(downId)= " + arr.remove(upId));
-					System.out.println("3up_list = " + down_list);
-					System.out.println("3downId = " + upId);
-					System.out.println("3up_list.split(downId) = " + down_list.split(upId));
-					System.out.println("3up_list.contains(downId) = " + down_list.contains(upId));
-					System.out.println("3downId.contains(up_list) = " + upId.contains(down_list));
-					System.out.println("3up_list.equals(downId) = " + down_list.equals(upId));
-					System.out.println("3downId.equals(up_list) = " + upId.equals(down_list));
+//				arr.add(upId);
+				for(int i = 0; i < arr_down_list.size(); i++) {
+					String str = arr_down_list.get(i);
+					arr_down_list.remove(str);
+					down_list = String.join(",", arr_down_list);
+
 				}
 				findComment.setComment_down_id_list(down_list);
 				findComment.setComment_up_id_list(null);
 				changeUpList(findComment);
 			}
 
-			String list = findComment.getComment_up_id_list();
 
 			if (list != null && list.contains(upId)) {
+
 				cEnt.setComment_recommend("-1");
 				cDao.recommendUpDown(cEnt);
-				ArrayList<String> arr = new ArrayList<>();
-				arr.add(upId);
-				for(int i = 0; i < arr.size(); i++) {
-					String str = arr.get(i);
-					if (str.equals(upId)) {
-						arr.remove(upId);
-					}
-					list = String.join(",", arr);
-					down_list = String.join(",", arr);
-					System.out.println("4arr = " + arr);
-					System.out.println("4str = " + str);
-					System.out.println("4str.equals(downId) = " + str.equals(upId));
-					System.out.println("4arr.remove(downId)= " + arr.remove(upId));
-					System.out.println("4up_list = " + down_list);
-					System.out.println("4downId = " + upId);
-					System.out.println("4up_list.split(downId) = " + down_list.split(upId));
-					System.out.println("4up_list.contains(downId) = " + down_list.contains(upId));
-					System.out.println("4downId.contains(up_list) = " + upId.contains(down_list));
-					System.out.println("4up_list.equals(downId) = " + down_list.equals(upId));
-					System.out.println("4downId.equals(up_list) = " + upId.equals(down_list));
+				for(int i = 0; i < arr_list.size(); i++) {
+					String str = arr_list.get(i);
+					arr_list.remove(str);
+					list = String.join(",", arr_list);
+
 				}
 				findComment.setComment_up_id_list(list);
 				findComment.setComment_down_id_list(null);
@@ -224,9 +166,6 @@ public class CommentController {
 			cDao.recommendUpDown(cEnt);
 			return -1;
 		}
-
-
-
 
 	}
 
