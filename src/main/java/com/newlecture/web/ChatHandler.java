22d207121390e18a94f968fd5138e174e676f7a0
tomhoon <gnsdl9108@naver.com>
2024-01-27 12:03:23 +0900

    feat: 채팅 기능 추가

---
+++ src/main/java/com/newlecture/web/ChatHandler.java
@@ -0,0 +1,71 @@
+package com.newlecture.web;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.newlecture.web.entity.ChatEntity;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ChatHandler extends TextWebSocketHandler {
+	
+	HashMap<String, WebSocketSession> sessionMap = new HashMap<>();
+	ChatEntity cEnt = new ChatEntity();
+	
+   // connection established
+   @Override
+   public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+	   log.info(">> afterConnectionEstablished " + session);
+	   	
+	   sessionMap.put(session.getId(), session);
+   }
+   
+   // message
+   @Override
+   protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+	   String msg = message.getPayload();
+	   ObjectMapper mapper = new ObjectMapper();
+	   cEnt = mapper.readValue(msg, ChatEntity.class);
+	   
+	   /**
+	    * ConcurrentModfiiacationException: null (동시 수정 오류) 
+	    * for문 -> iteration 사용
+	    */
+	   
+	   Set<String> set = new HashSet<>();
+	   set = sessionMap.keySet();
+	   Iterator<String> it = set.iterator();
+	   
+	   while (it.hasNext()) {
+		   WebSocketSession wss = sessionMap.get(it.next());
+		   try {
+//			   wss.sendMessage(new TextMessage(msg));
+			   
+			   String msgObject = mapper.writeValueAsString(cEnt);
+			   wss.sendMessage(new TextMessage(msgObject));
+		   } catch (Exception e) {
+//			   synchronized (sessionMap) {
+//				   sessionMap.remove(it.next());
+//			   }
+		   }
+	   }
+	   log.info(">> handleTextMessage " + message.getPayload());
+   }
+
+   // connection closed
+   @Override
+   public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+	   log.info(">> afterConnectionClosed " + status);
+   }
+}
---
+++ src/main/java/com/newlecture/web/entity/ChatEntity.java
@@ -0,0 +1,19 @@
+package com.newlecture.web.entity;
+
+public class ChatEntity {
+	String chatId;
+	String chatContent;
+	
+	public String getChatId() {
+		return chatId;
+	}
+	public void setChatId(String chatId) {
+		this.chatId = chatId;
+	}
+	public String getChatContent() {
+		return chatContent;
+	}
+	public void setChatContent(String chatContent) {
+		this.chatContent = chatContent;
+	}
+}
