fee538858e8d7e991bcaa634bedb0c06fe5a07fb
남병현 <kmi5063@naver.com>
2024-02-03 15:47:11 +0900

    chore : 카카오로그인 삭제

---
+++ src/main/java/com/newlecture/web/controller/KakaoController.java
@@ -0,0 +1,15 @@
+package com.newlecture.web.controller;
+
+import com.newlecture.web.service.GetKakaoAccessToken;
+import com.newlecture.web.service.GetUserInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.json.JSONObject;
+
+@Controller
+public class KakaoController {
+
+
+}
